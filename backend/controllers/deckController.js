575dc20d497c649102f911858e76a5bf34b5ebdd
CaveSpider1212 <aabhwan.adhikary@gmail.com>
2025-08-07 00:37:07 -0700

    Added data validation and error handling to controllers

--- backend/controllers/deckController.js
+++ backend/controllers/deckController.js
@@ -7,40 +7,61 @@ const getDecks = async (req, res) => {
 };
 
 const getDeckById = async (req, res) => {
-    const deck = await Deck.findById(req.params.id);
+    try {
+        const deck = await Deck.findById(req.params.id);
 
-    if (!deck) {
-        res.status(404);
-        throw("Deck not found!");
+        if (!deck) {
+            res.status(404);
+            throw("Deck not found!");
+        }
+
+        res.status(201).json(deck);
+    } catch (err) {
+        console.log(err);
     }
-
-    res.status(201).json(deck);
 }
 
 const updateDeck = async (req, res) => {
-    const deck = await Deck.findById(req.params.id);
+    try {
+        const deck = await Deck.findById(req.params.id);
 
-    if (!deck) {
-        res.status(404);
-        throw("Deck not found!");
+        if (!deck) {
+            res.status(404);
+            throw("Deck not found!");
+        }
+
+        const newDeck = await Deck.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+        res.status(200).json(newDeck);
+    } catch (err) {
+        console.log(err);
     }
-
-    const newDeck = await Deck.findByIdAndUpdate(req.params.id, req.body, {new: true});
-
-    res.status(200).json(newDeck);
 }
 
 const createDeck = async (req, res) => {
-    const {name, description} = req.body;
-    const newDeck = await Deck.create({name, description});
-    res.status(201).json(newDeck);
+    try {
+        const {name, description} = req.body;
+
+        if (!name || !description) {
+            res.status(400).json({message: "Name and description both required"});
+        }
+
+        const newDeck = await Deck.create({name, description});
+        res.status(201).json(newDeck);
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 const deleteDeck = async (req, res) => {
-    await Flashcard.deleteMany({deck: req.params.id});
-    await Deck.findByIdAndDelete(req.params.id);
+    try {
+        await Flashcard.deleteMany({deck: req.params.id});
+        await Deck.findByIdAndDelete(req.params.id);
 
-    res.status(201).send(`Deleted deck with ID: ${req.params.id}`);
+        res.status(201).send(`Deleted deck with ID: ${req.params.id}`);
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 module.exports = {getDecks, getDeckById, updateDeck, createDeck, deleteDeck};
--- backend/controllers/flashcardController.js
+++ backend/controllers/flashcardController.js
@@ -2,40 +2,55 @@ const Flashcard = require("../models/flashcardModel");
 const Deck = require("../models/deckModel");
 
 const createFlashcard = async (req, res) => {
-    const deckId = req.params.id;
-    const {term, definition} = req.body;
+    try {
+        const deckId = req.params.id;
+        const {term, definition} = req.body;
 
-    const newCard = await Flashcard.create({term, definition, deck: deckId});
-    const savedCard = await newCard.save();
+        if (!term || !definition) {
+            res.status(400).json({message: "Term and definition both need to be filled."});
+        }
 
-    await Deck.findByIdAndUpdate(req.params.id, {$push: {cards: savedCard._id}});
-     
-    res.status(201).json(savedCard);
+        const newCard = await Flashcard.create({term, definition, deck: deckId});
+        const savedCard = await newCard.save();
+
+        await Deck.findByIdAndUpdate(req.params.id, {$push: {cards: savedCard._id}});
+        
+        res.status(201).json(savedCard);
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 const updateFlashcard = async (req, res) => {
-    const card = await Flashcard.findById(req.params.id);
+    try {
+        const newCard = await Flashcard.findByIdAndUpdate(req.params.id, req.body, {new: true});
 
-    if (!card) {
-        res.status(404);
-        throw("Card not found!");
+        if (!newCard) {
+            res.status(404);
+            throw("Card not found!");
+        }
+
+        res.status(200).json(newCard);
+    } catch (err) {
+        console.log(err);
     }
-
-    const newCard = await Flashcard.findByIdAndUpdate(req.params.id, req.body, {new: true});
-    res.status(200).json(newCard);
 }
 
 const deleteFlashcard = async (req, res) => {
-    const deleteCard = await Flashcard.findByIdAndDelete(req.params.id);
+    try {
+        const deleteCard = await Flashcard.findByIdAndDelete(req.params.id);
 
-    if (!deleteCard) {
-        res.status(404);
-        throw("Card not found!");
+        if (!deleteCard) {
+            res.status(404);
+            throw("Card not found!");
+        }
+        
+        await Deck.findByIdAndUpdate(deleteCard.deck, {$pull: {cards: deleteCard._id}});
+
+        res.status(201).send(`Deleted card with ID: ${req.params.id}`);
+    } catch (err) {
+        console.log(err);
     }
-    
-    await Deck.findByIdAndUpdate(deleteCard.deck, {$pull: {cards: deleteCard._id}});
-
-    res.status(201).send(`Deleted card with ID: ${req.params.id}`);
 };
 
 module.exports = {createFlashcard, updateFlashcard, deleteFlashcard};
