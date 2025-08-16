0f7da475ebb1d6b7aa0e98b1d21210377be7bded
CaveSpider1212 <aabhwan.adhikary@gmail.com>
2025-08-16 13:42:07 -0700

    Updated navigation bar to show different message if user is logged in

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom"
+import { useState, useEffect } from "react";
+import { getCurrentUser } from "../api";
 import "../css/NavBar.css";
 
 /**
@@ -6,9 +8,33 @@ import "../css/NavBar.css";
  */
 
 function NavBar () {
+    /**
+     * State variables
+     */
+    const [user, setUser] = useState(null); // represents the user logged in, set to null by default
+
+
+    /**
+     * Looks for the token in the local storage
+     * If found, then set the user state variable to the user associated with the token (using getCurrentUser())
+     */
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            getCurrentUser(token).then((data) => setUser(data));
+        }
+        else {
+            setUser(null);
+        }
+
+        // window.location.reload(); // refreshes the page after login/logout (to display change)
+    });
+
 
     /**
      * The <nav> element consists of the app title ("Flashcards App") and <LinK> elements routing to the different pages of the application
+     * If user is logged in, then show "Logged in as {user}"; otherwise, show "Login/Register"
      */
     return (
         <nav>
@@ -20,7 +46,11 @@ function NavBar () {
             </div>
 
             <div className="right-side">
-                <Link to="/account" className="links account">Login/Register</Link>
+                {user ? (
+                    <Link to="/account" className="links account">Logged in as {user.username}</Link>
+                ) : (
+                    <Link to="/account" className="links account">Login/Register</Link>
+                )}
             </div>
         </nav>
     )
