4b0111d33a030cb89c73cd4a21f4c03d89743b6e
CaveSpider1212 <aabhwan.adhikary@gmail.com>
2025-08-18 00:53:50 -0700

    Modified deck routes file

--- backend/routes/deckRoutes.js
+++ backend/routes/deckRoutes.js
@@ -9,11 +9,11 @@ const Deck = require("../models/deckModel");
 // CREATE ROUTER
 const router = express.Router();
 
-// ROUTES -- *** require user authentication/login
-router.get("/", validateToken, getDecks);
-router.get("/:id", validateToken, getDeckById);
+// ROUTES
+router.get("/", validateToken, getDecks); // requires user authentication/login
+router.get("/:id", getDeckById);
 router.put("/:id", validateToken, isOwner(Deck), updateDeck); // verifies the logged-in user owns the deck being modified
-router.post("/", validateToken, createDeck);
+router.post("/", validateToken, createDeck); // requires user authentication/login
 router.delete("/:id", validateToken, isOwner(Deck), deleteDeck); // verifies the logged-in user owns the deck being modified
 
 // EXPORTS: router
