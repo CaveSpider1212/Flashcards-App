dc9d8421facf2e58a750fa9a026605de45e3273d
CaveSpider1212 <aabhwan.adhikary@gmail.com>
2025-08-20 15:25:58 -0700

    Fixed CORS use function

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: process.env.CLIENT_URL
+    origin: [
+        "http://localhost:5173",
+        process.env.CLIENT_URL
+    ],
+
+    credentials: true
 }));
 app.use(express.json());
 
